04ecc071063569dd6b33d95a9bb62d59eb0babaa
dreik84 <dreik84@mail.ru>
2025-09-11 15:56:32 +0300

    Update Tree.java

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -52,6 +52,58 @@ class Tree {
                 }
             }
         }
+    }
 
+    public boolean delete(int key) {
+        Node current = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+
+        while (current.iData != key) {
+            parent = current;
+
+            if (key < current.iData) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+
+            if (current == null) return false;
+
+            if (current.leftChild == null && current.rightChild == null) {
+
+                if (current == root) root = null;
+                else if (isLeftChild) parent.leftChild = null;
+                else parent.rightChild = null;
+            }
+        }
+
+        return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
