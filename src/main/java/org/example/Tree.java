9f4fe1e8914335bf705c5f473baaf970ed368f9a
dreik84 <dreik84@mail.ru>
2025-09-12 15:56:05 +0300

    Update Tree.java

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -77,11 +77,50 @@ class Tree {
                 if (current == root) root = null;
                 else if (isLeftChild) parent.leftChild = null;
                 else parent.rightChild = null;
+
+            } else if (current.rightChild == null) {
+
+                if (current == root) root = current.leftChild;
+                else if (isLeftChild) parent.leftChild = current.leftChild;
+                else parent.rightChild = current.leftChild;
+
+            } else if (current.leftChild == null) {
+
+                if (current == root) root = current.rightChild;
+                else if (isLeftChild) parent.leftChild = current.rightChild;
+                else parent.rightChild = current.rightChild;
+
+            } else {
+
+                Node successor = getSuccessor(current);
+
+                if (current == root) root = successor;
+                else if (isLeftChild) parent.leftChild = successor;
+                else parent.rightChild = successor;
             }
         }
-
         return true;
     }
+
+    private Node getSuccessor(Node delNode) {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.rightChild;
+
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+
+        if (successor != delNode.rightChild) {
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+
+        return successor;
+    }
+
 }
 
 
