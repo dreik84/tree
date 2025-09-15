8b64260fb2268272e35cd4d5d278d58be60cd9b7
dreik84 <dreik84@mail.ru>
2025-09-15 16:07:32 +0300

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,68 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        int value;
+        Tree theTree = new Tree();
+
+        theTree.insert(50, 1.5);
+        theTree.insert(25, 1.2);
+        theTree.insert(75, 1.7);
+        theTree.insert(12, 1.5);
+        theTree.insert(37, 1.2);
+        theTree.insert(43, 1.7);
+        theTree.insert(30, 1.5);
+        theTree.insert(33, 1.2);
+        theTree.insert(87, 1.7);
+        theTree.insert(93, 1.5);
+        theTree.insert(97, 1.5);
+
+        while (true) {
+            System.out.print("Enter first letter of show, ");
+            System.out.print("insert, find, delete, or traverse: ");
+
+            int choice = getChar();
+
+            switch (choice) {
+                case 's':
+                    theTree.displayTree();
+                    break;
+                case 'i':
+                    System.out.print("Enter value to insert: ");
+                    value = getInt();
+                    theTree.insert(value, value + 0.9);
+                    break;
+                case 'f':
+                    System.out.print("Enter value to find: ");
+                    value = getInt();
+                    Node found = theTree.find(value);
+
+                    if (found != null) {
+                        System.out.print("Found: ");
+                        found.displayNode();
+                        System.out.println();
+                    }
+            }
+        }
+    }
+
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        return br.readLine();
+    }
+
+    public static char getChar() throws IOException {
+        String str = getString();
+        return str.charAt(0);
+    }
+
+    public static int getInt() throws IOException {
+        String str = getString();
+        return Integer.parseInt(str);
     }
 }
